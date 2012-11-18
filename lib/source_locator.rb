d738d5f29a77f33f125d0ae59caf25f5d127465b
Unnikrishnan KP <unni.tallman@gmail.com>
2012-11-18 14:58:12 +0530

    bug fix and version bump

--- lib/source_locator.rb
+++ lib/source_locator.rb
@@ -1,4 +1,5 @@
 require "source_locator/version"
+require "source_locator/class_loader"
 require "source_locator/dictionary"
 require "source_locator/locater"
 require "source_locator/editors/sublime_text"
